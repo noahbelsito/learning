e3243e757921f17d263e9dbb28e2be371717d42c
Noah Belsito <32454695+noahbelsito@users.noreply.github.com>
2021-10-18 22:06:40 -0400

    python basics: operations, types, and printing

---
+++ README.md
@@ -0,0 +1,7 @@
+# Learning Machine Learning
+I have an extreme interest in the future of robots that have feelings.
+Not the idea of looking like having emotions but the actual feeling.
+The feeling you get when you lose someone you love. 
+The feeling you get when you feel in love.
+To feel. Not just to seem hurt.
+A repo to watch my journey. Thanks for stopping by. ❤️
---
+++ python/hello.py
@@ -0,0 +1,32 @@
+# A variable stores data
+num = 1
+# Prints data to console
+print(num)
+
+# Operations change the value of variables or data
+num_2 = num + 12
+
+# Conditional statements run code based on whether the condition passes or fails
+if num_2 > 12:
+   print("Number is greater than 12.")
+
+# String concatenation operates on strings to create new strings
+string = "This is num: " + num + ", and this is num_2: " + num_2 + "."
+
+# Number variable types
+"""
+   int = whole number
+   float = non-whole number
+"""
+
+# Operations
+"""
+   a + b   |   Addition           |   Sum of a and b
+   a - b   |   Subtraction 	      |   Difference of a and b
+   a * b   |   Multiplication     |   Product of a and b
+   a / b   |   True division      |   Quotient of a and b
+   a // b  |   Floor division     |   Quotient of a and b, removing fractional parts
+   a % b   |   Modulus 	Integer   |   Remainder after division of a by b
+   a ** b  |   Exponentiation 	  |   A raised to the power of b
+   -a      |   Negation 	      |   The negative of a
+"""
