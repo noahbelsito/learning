5c7e9219ae3a5d6e99bc3ec71ffa3d0fd2048309
Noah Belsito <32454695+noahbelsito@users.noreply.github.com>
2021-10-26 18:04:11 -0400

    added model mae comparisons

--- intro_to_ml/random_forests.py
+++ intro_to_ml/random_forests.py
@@ -12,10 +12,21 @@ from sklearn.metrics import mean_absolute_error
 
 music_csv = "music.csv"
 music_data = pd.read_csv(music_csv)
+music_data = music_data.dropna(axis=0)
 y = music_data["artist.familiarity"]
 features = ["artist.hotttnesss", "song.bars_confidence", "song.duration", "song.hotttnesss", "song.key_confidence", "song.loudness", "song.tempo", "song.time_signature"]
 X = music_data[features]
 train_X, val_X, train_y, val_y = train_test_split(X, y)
+
+no_split_model = DecisionTreeRegressor(random_state=1)
+no_split_model.fit(X, y)
+no_split_predictions = no_split_model.predict(X)
+print("No Split Model: ", mean_absolute_error(y, no_split_predictions))
+
+tree_model = DecisionTreeRegressor(random_state=1)
+tree_model.fit(train_X, train_y)
+tree_predictions = tree_model.predict(val_X)
+print("Tree Model: ", mean_absolute_error(val_y, tree_predictions))
 
 forest_model = RandomForestRegressor(random_state=1)
 forest_model.fit(train_X, train_y)
