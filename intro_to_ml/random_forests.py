df31453e7598dca2fdbee35ab5078d0f49e47d92
Noah Belsito <32454695+noahbelsito@users.noreply.github.com>
2021-10-26 17:57:40 -0400

    random forest models

---
+++ intro_to_ml/random_forests.py
@@ -0,0 +1,23 @@
+# Deep trees with lots of leaves will over fit because each prediction is coming from historical data from a few values
+# Shallow trees will perform poorly because it fails to capture as many distinctions in the raw data
+# Random forest uses many trees and it can make a prediction by averaging out the predictions of each component tree
+# these random forests are usually considerably more accurate than most single tree models
+
+import pandas as pd
+
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+
+music_csv = "music.csv"
+music_data = pd.read_csv(music_csv)
+y = music_data["artist.familiarity"]
+features = ["artist.hotttnesss", "song.bars_confidence", "song.duration", "song.hotttnesss", "song.key_confidence", "song.loudness", "song.tempo", "song.time_signature"]
+X = music_data[features]
+train_X, val_X, train_y, val_y = train_test_split(X, y)
+
+forest_model = RandomForestRegressor(random_state=1)
+forest_model.fit(train_X, train_y)
+forest_predictions = forest_model.predict(val_X)
+print("Forest Model: ", mean_absolute_error(val_y, forest_predictions))
