91875fadafb63ea531ef37347747ab921efa36ac
Noah Belsito <32454695+noahbelsito@users.noreply.github.com>
2021-10-29 23:58:12 -0400

    combining

--- pandas/renaming_combining.py
+++ pandas/renaming_combining.py
@@ -12,3 +12,10 @@ example_data.rename(index={0: 'firstEntry', 1: 'secondEntry'})
 example_data.rename_axis('Unnamed', axis='rows').rename_axis(0, axis='columns')
 
 print(example_data)
+
+example_data_2 = pd.read_csv('example.csv')
+combined_data = pd.concat([example_data, example_data_2])
+print(combined_data)
+
+combined_data = example_data.join(example_data_2, lsuffix='_1', rsuffix='_2')
+print(combined_data)
